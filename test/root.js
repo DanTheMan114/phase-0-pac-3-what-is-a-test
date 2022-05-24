86f8ebd7ff045053fbf0262906d11ec31a5e7a52
Daniel Turner <danielturner184@gmail.com>
2022-05-23 22:27:29 -0700

    Done.

--- test/root.js
+++ test/root.js
@@ -3,7 +3,9 @@ global.expect = require('expect')
 const fs = require('fs')
 const jsdom = require('mocha-jsdom')
 const path = require('path')
+const expect = require('expect/lib')
 
 jsdom({
   src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
 })
+
